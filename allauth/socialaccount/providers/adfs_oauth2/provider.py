a2fe033d0ca4c2d8b7009decab4913886ac12c74
gordon <wgordonw1@gmail.com>
2015-06-12 09:28:20 -0400

    decode the objectGUID to match the value shown in active directory

--- allauth/socialaccount/providers/adfs_oauth2/provider.py
+++ allauth/socialaccount/providers/adfs_oauth2/provider.py
@@ -1,6 +1,7 @@
 from allauth.socialaccount import providers
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+from uuid import UUID
 
 
 class ADFSOAuth2Account(ProviderAccount):
@@ -14,7 +15,7 @@ class ADFSOAuth2Provider(OAuth2Provider):
     account_class = ADFSOAuth2Account
 
     def extract_uid(self, data):
-        return data['ppid']
+        return UUID(bytes_le=data['ppid'].decode("base64")).__str__()
 
     def extract_common_fields(self, data):
         return dict(name=data.get('unique_name'))
