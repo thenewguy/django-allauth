4ad18357590f7e9cb8281fc66ac2722e91f7f3ef
gordon <wgordonw1@gmail.com>
2015-06-13 16:11:48 -0400

    use a signal to extract user data from the adfs response so the implementer can disconnect the defaults and connect a site specific version if necessary

--- allauth/socialaccount/providers/adfs_oauth2/provider.py
+++ allauth/socialaccount/providers/adfs_oauth2/provider.py
@@ -1,8 +1,8 @@
+from django.core.exceptions import ImproperlyConfigured
 from allauth.socialaccount import providers
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
-from allauth.account.models import EmailAddress
-from uuid import UUID
+from .signals import extract_uid_signal, extract_common_fields_signal, extract_email_addresses_signal
 
 class ADFSOAuth2Account(ProviderAccount):
     pass
@@ -15,20 +15,26 @@ class ADFSOAuth2Provider(OAuth2Provider):
     @property
     def name(self):
         return self.get_settings().get("name", "ADFS Oauth2")
-
+    
+    def signal_responses_handler(self, label, responses):
+        responses_len = len(responses)
+        if not responses_len:
+            raise ImproperlyConfigured("No `%s` receivers responded." % label)
+        elif 1 < responses_len:
+            senders = [response[1] for response in responses]
+            raise ImproperlyConfigured("Too many `%s` receivers responded... THERE CAN ONLY BE ONE!  Disconnect unwanted signals.  Reference `https://docs.djangoproject.com/en/1.7/topics/signals/#django.dispatch.Signal`.  Detected the following senders: %s" % (label, ", ".join(senders)))
+        return responses[0][1]
+    
     def extract_uid(self, data):
-        raw = data.get('ppid').decode("base64")
-        uid = UUID(bytes_le=raw)
-        return unicode(uid)
+        responses = extract_uid_signal.send(sender=self, data=data)
+        return self.signal_responses_handler("extract_uid", responses)
 
     def extract_common_fields(self, data):
-        return dict(
-            username = data.get('upn').split("@")[0],
-            first_name = data.get('given_name'),
-            last_name = data.get('family_name'),
-        )
+        responses = extract_common_fields_signal.send(sender=self, data=data)
+        return self.signal_responses_handler("extract_common_fields", responses)
     
     def extract_email_addresses(self, data):
-        return [EmailAddress(email=data.get('upn'), verified=True, primary=True)]
+        responses = extract_email_addresses_signal.send(sender=self, data=data)
+        return self.signal_responses_handler("extract_email_addresses", responses)
 
 providers.registry.register(ADFSOAuth2Provider)
